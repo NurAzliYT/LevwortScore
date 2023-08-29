172c0b42364ad780d24c35f7a124a2ac45398867
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2023-08-29 22:20:18 +0700

    Create ScoreUpdateTitleTask.php

---
+++ src/NurAzliYT/LevwortScore/task/ScoreUpdateTitleTask.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types = 1);
+
+namespace NurAzliYT\LevwortScore\task;
+
+use NurAzliYT\LevwortScore\LevwortScore;
+use NurAzliYT\LevwortScore\session\PlayerManager;
+use pocketmine\scheduler\Task;
+use function is_null;
+
+class ScoreUpdateTitleTask extends Task{
+
+	public function __construct(private Levwort $plugin){}
+
+	public function onRun() : void{
+		foreach($this->plugin->getServer()->getOnlinePlayers() as $player){
+			if(is_null($session = PlayerManager::get($player))){
+				continue;
+			}
+
+			$session->handle(null, true);
+		}
+	}
+}
