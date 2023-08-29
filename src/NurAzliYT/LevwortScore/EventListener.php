485a39678fcc6435be0536ed9975fa2f5e1a778f
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2023-08-29 20:12:36 +0700

    Create EventListener.php

---
+++ src/NurAzliYT/LevwortScore/EventListener.php
@@ -0,0 +1,89 @@
+<?php
+declare(strict_types = 1);
+
+namespace NurAzliYT\LevwortScore;
+
+use Ifera\LevwortScore\event\PlayerTagsUpdateEvent;
+use Ifera\LevwortScore\event\PlayerTagUpdateEvent;
+use Ifera\LevwortScore\event\ServerTagsUpdateEvent;
+use Ifera\LevwortScore\event\ServerTagUpdateEvent;
+use Ifera\LevwortScore\scoreboard\ScoreTag;
+use Ifera\LevwortScore\session\PlayerManager;
+use pocketmine\event\entity\EntityTeleportEvent;
+use pocketmine\event\Listener;
+use pocketmine\player\Player;
+use function is_null;
+
+class EventListener implements Listener {
+
+	/** @var LevwortScore */
+	private $plugin;
+
+	public function __construct(LevwortScore $plugin) {
+		$this->plugin = $plugin;
+	}
+
+	public function onWorldChange(EntityTeleportEvent $event): void {
+		if (!LevwortScoreSettings::isMultiWorld()) {
+			return;
+		}
+
+		$from = $event->getFrom();
+		$to = $event->getTo();
+
+		if ($from->getWorld()->getFolderName() === $to->getWorld()->getFolderName()) {
+			return;
+		}
+
+		$player = $event->getEntity();
+
+		if (!$player instanceof Player or !$player->spawned) {
+			return;
+		}
+
+		PlayerManager::getNonNull($player)->handle($to->getWorld()->getFolderName());
+	}
+
+	public function onServerTagUpdate(ServerTagUpdateEvent $event): void {
+		$this->updateServerTag($event->getTag());
+	}
+
+	public function onServerTagsUpdate(ServerTagsUpdateEvent $event): void {
+		foreach ($event->getTags() as $tag) {
+			$this->updateServerTag($tag);
+		}
+	}
+
+	public function onPlayerTagUpdate(PlayerTagUpdateEvent $event): void {
+		$this->updateTag($event->getPlayer(), $event->getTag());
+	}
+
+	public function onPlayerTagsUpdate(PlayerTagsUpdateEvent $event): void {
+		foreach ($event->getTags() as $tag) {
+			$this->updateTag($event->getPlayer(), $tag);
+		}
+	}
+
+	private function updateServerTag(ScoreTag $tag): void {
+		foreach (PlayerManager::getAll() as $session) {
+			$this->updateTag($session->getPlayer(), $tag);
+		}
+	}
+
+	private function updateTag(Player $player, ScoreTag $newTag): void {
+		if (
+			!$player->isOnline() ||
+			LevwortScoreSettings::isInDisabledWorld($player->getWorld()->getFolderName()) ||
+			is_null($session = PlayerManager::get($player)) ||
+			is_null($scoreboard = $session->getScoreboard()) ||
+			is_null($scoreTag = $scoreboard->getTag($newTag->getName()))
+		) {
+			return;
+		}
+
+		$scoreTag->setValue($newTag->getValue());
+
+		if (LevwortScoreSettings::isSingleLineUpdateMode()) $scoreboard->handleSingleTagUpdate($scoreTag);
+		else $scoreboard->update()->display();
+	}
+}
