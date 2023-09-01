193d6a198975eebcc52187ad4b9f4c64b06e52e4
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2023-09-01 18:18:47 +0700

    Update UpdateNotifierTask.php

--- src/JackMD/UpdateNotifier/task/UpdateNotifierTask.php
+++ src/JackMD/UpdateNotifier/task/UpdateNotifierTask.php
@@ -10,7 +10,7 @@ use function json_decode;
 use function version_compare;
 use function vsprintf;
 
-namespace UpdateNotifyTask extends AsyncTask {
+class UpdateNotifyTask extends AsyncTask {
 
 	/** @var string */
 	private const POGGIT_RELEASES_URL = "https://poggit.pmmp.io/releases.min.json?name=";
